9637592f8075ad3611d0956c8a0d089323b64ea2
Amr Khaled <lewolfje@gmail.com>
2024-05-22 10:51:34 +0300

    add middleware and protect route

--- src/features/notification/mail.routes.ts
+++ src/features/notification/mail.routes.ts
@@ -2,12 +2,16 @@ import { Router } from "express";
 
 import { asyncHandler } from "@fcai-sis/shared-utilities";
 import sendEmailHandler from "./logic/handlers/sendEmail.handler";
+import { Role, checkRole } from "@fcai-sis/shared-middlewares";
+import validateSendEmailRequestBodyMiddleware from "./logic/middlewares/validateEmailRequestBody.middleware";
 
 export default (router: Router) => {
   router.post(
     "/email",
 
-    // Handle example request
+    checkRole([Role.EMPLOYEE, Role.ADMIN]),
+    validateSendEmailRequestBodyMiddleware,
+
     asyncHandler(sendEmailHandler)
   );
 };
